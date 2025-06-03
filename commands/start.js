ffaaa16b5011802a2d20268a1e285f52884ab9a6
andre <andrebeet1@gmail.com>
2025-06-03 15:27:03 +0200

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,10 +1,14 @@
-module.exports = (ctx) => {
-    ctx.reply('Bienvenue dans le Guide Spirituel 🙏\n\nUtilisez /prayer pour recevoir une prière.\nOu cliquez sur un bouton ci-dessous.', {
+module.exports = async (ctx) => {
+  await ctx.reply(
+    `🙏 Bienvenue dans ton guide spirituel ! Que puis-je faire pour toi aujourd'hui ?`,
+    {
       reply_markup: {
         inline_keyboard: [
-          [{ text: "Poser une question spirituelle", callback_data: "ask_question" }]
+          [{ text: "🧘 Pose une question spirituelle", callback_data: "ask_question" }],
+          [{ text: "🙏 Demande de prière", callback_data: "prayer_request" }],
+          [{ text: "❓ Aide / Informations", callback_data: "help_info" }]
         ]
       }
-    });
-  };
-  
+    }
+  );
+};

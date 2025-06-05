a563839d2a783cd923d042a8b4eb28332c06a4d7
andre <andrebeet1@gmail.com>
2025-06-05 15:20:08 +0200

    Update note.js

--- commands/note.js
+++ commands/note.js
@@ -1,11 +1,16 @@
+const { Markup } = require('telegraf');
+
 module.exports = async (ctx) => {
   const message = `
 📝 *Note du jour*
 
-Clique ici pour lire la méditation spirituelle du jour : 
-👉 [Lire la note du jour](https://anno-jeik.onrender.com/)
-
 Que cette lecture t'inspire et t'encourage dans ta marche spirituelle 🙏
 `;
-  await ctx.replyWithMarkdown(message);
+
+  await ctx.replyWithMarkdown(
+    message,
+    Markup.inlineKeyboard([
+      Markup.button.url('Lire la note du jour', 'https://anno-jeik.onrender.com/')
+    ])
+  );
 };
